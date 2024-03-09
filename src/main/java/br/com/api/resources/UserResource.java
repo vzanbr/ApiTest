86219a193cc54bf0d7b259aea3e172fc5f11ae9e
vzanbr <128941678+vzanbr@users.noreply.github.com>
2024-03-09 14:01:47 -0300

    feature: Autowired de service no resource!

--- src/main/java/br/com/api/resources/UserResource.java
+++ src/main/java/br/com/api/resources/UserResource.java
@@ -1,19 +1,25 @@
 package br.com.api.resources;
 
 import br.com.api.domain.User;
+import br.com.api.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/user")
 public class UserResource {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById (@PathVariable Integer id) {
-        return ResponseEntity.ok().body
-                (new User(1, "Gabriel", "123@gamil.com", "123"));
+        return ResponseEntity.ok().body(userService.findById(id));
     }
+
+//    @PostMapping(value = "/criar")
+//    public void  criar (@RequestBody User user) {
+//
+//    }
 }
