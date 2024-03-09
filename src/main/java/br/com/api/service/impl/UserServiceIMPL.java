cf7c4d4dbaa3c046b99ce41469982ac628d12859
vzanbr <128941678+vzanbr@users.noreply.github.com>
2024-03-09 13:34:34 -0300

    feat : classe user service impl findById criado

---
+++ src/main/java/br/com/api/service/impl/UserServiceIMPL.java
@@ -0,0 +1,22 @@
+package br.com.api.service.impl;
+
+import br.com.api.domain.User;
+import br.com.api.repository.UserRepository;
+import br.com.api.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceIMPL implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public User findById(Integer id) {
+        Optional<User> obj = userRepository.findById(id);
+        return obj.orElse(null);
+    }
+}
