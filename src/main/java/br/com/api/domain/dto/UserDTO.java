30e70448d46f6b78e5279ee4e014c3c698babe51
vzanbr <128941678+vzanbr@users.noreply.github.com>
2024-03-09 15:14:09 -0300

    feature: implementado classe DTO

---
+++ src/main/java/br/com/api/domain/dto/UserDTO.java
@@ -0,0 +1,16 @@
+package br.com.api.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+
+    private Integer id;
+    private String name;
+    private String email;
+    private String password;
+}
