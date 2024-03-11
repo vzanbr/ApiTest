139ae76157a0a84212b7c1a7606e69030af52f85
vzanbr <128941678+vzanbr@users.noreply.github.com>
2024-03-10 23:45:40 -0300

    fix : Removendo bug

--- src/main/java/br/com/api/domain/dto/UserDTO.java
+++ src/main/java/br/com/api/domain/dto/UserDTO.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class UserDTO {
 
     private Integer id;
--- src/test/java/br/com/api/resources/UserResourceTest.java
+++ src/test/java/br/com/api/resources/UserResourceTest.java
@@ -34,8 +34,8 @@ class UserResourceTest {
     private static final String EMAIL = "gabriecvr123@gmail.com";
     private static final String PASSWORD = "123";
 
-    private User user;
-    private UserDTO userDTO;
+    private User user = new User();
+    private UserDTO userDTO = new UserDTO();
 
     @InjectMocks
     private UserResource resource;
